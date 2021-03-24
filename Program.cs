f8212e19d9e9b1070ef00e9b50db79ebf6f4138e
Luis Emilio Pimentel Mora <79173414+Mesifruit@users.noreply.github.com>
2021-03-23 23:31:30 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Parcial
+{
+    class pregunta12
+    {
+        string[] n;
+        float[] s;
+        public void parcial()
+        {
+            n = new string[5];
+            s = new float[5];
+            for (int h = 0; h < n.Length; h++)
+            {
+
+                Console.Write("Nombre del empleado: ");
+                n[h] = Console.ReadLine();
+                Console.Write("Salario del empleado: ");
+                string z;
+                z = Console.ReadLine();
+
+                s[h] = float.Parse(z);
+            }
+        }
+        public void MayorS()
+        {
+
+            float nm; int k = 0; nm = s[0];
+
+            for (int h = 1; h < n.Length; h++)
+            {
+                if (s[h] > nm)
+                {
+                    nm = s[h];
+                    k = h;
+                }
+            }
+            Console.WriteLine("El empleado que devenga un mayor salario es: " + n[k]);
+            Console.WriteLine("Cuyo salario es de: " + nm);
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            pregunta12 pv = new pregunta12();
+            pv.parcial();
+            pv.MayorS();
+        }
+    }
+}
+
